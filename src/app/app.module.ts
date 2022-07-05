9af6468cdbc4da86d2f80ddbff30569f8a43ec8c
Tencks <robertinospini@hotmail.com>
2022-07-04 21:50:53 -0300

    Carga del projecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginasComponent } from './paginas/paginas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginasComponent
   ],
   imports: [
     BrowserModule,
